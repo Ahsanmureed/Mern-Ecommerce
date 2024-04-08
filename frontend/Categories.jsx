0fdf4c874b584adc822a2a8d7a6b5495735075f3
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-04-08 16:24:39 +0500

    Add files via upload

---
+++ frontend/Categories.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import useCategory from '../hooks/useCategory'
+import { Link } from 'react-router-dom';
+
+const Categories = () => {
+    const categories= useCategory()
+   
+  return (
+    <div className=' mt-24'>
+        <div className='  grid grid-cols-2  md:grid-cols-3 px-6 md:px-28 gap-6 '>
+            {
+                categories?.map((category)=>(
+                    <Link  key={category._id} to={`/category/${category.slug}`}><div className=' w-[40vw] md:w-[20vw] border-2 px-4 py-20 flex rounded-md bg-blue-500  text-white text-3xl font-poppins font-semibold    justify-center'><h1>{category.name}</h1></div></Link>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Categories
