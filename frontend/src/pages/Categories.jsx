635032417c2c2cfccde0616e40f5ef3163d1aa95
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-04-08 16:15:16 +0500

    Add files via upload

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -7,7 +7,7 @@ const Categories = () => {
    
   return (
     <div className=' mt-24'>
-        <div className='  grid grid-cols-2 md:grid-cols-3 px-8 md:px-28 gap-6 '>
+        <div className='  grid grid-cols-2  md:grid-cols-3 px-12 md:px-28 gap-6 '>
             {
                 categories?.map((category)=>(
                     <Link  key={category._id} to={`/category/${category.slug}`}><div className=' w-[40vw] md:w-[20vw] border-2 px-4 py-20 flex rounded-md bg-blue-500  text-white text-3xl font-poppins font-semibold    justify-center'><h1>{category.name}</h1></div></Link>
