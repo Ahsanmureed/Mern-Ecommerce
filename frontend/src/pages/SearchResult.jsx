8fb694c74bc53270f515003467e51da91a6b0c90
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-05-15 21:39:51 +0500

    Add files via upload

--- frontend/src/pages/SearchResult.jsx
+++ frontend/src/pages/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { SearchContext } from '../Context/SearchContext'
 import Product from '../components/Product'
 
