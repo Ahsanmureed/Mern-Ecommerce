e288db6a576d86970d4ff829c23f02a05b5e1eb2
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-05-18 13:10:56 +0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,27 @@ import app from "./app.js";
 import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import multer from "multer"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // DataBase Connection
 import connection from "./DataBase/DbConnection.js";
 connection();
 // Env config
 import dotenv from "dotenv"
 dotenv.config();
+
+//  middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:"https://mern-ecommerce-dqm5.vercel.app",
-    credentials:true
+    origin:["https://mern-ecommerce-dqm5.vercel.app","http://localhost:5173"],
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials:true,
+    
 }))
 // userRoutes
 import categoryRouter from "./routes/categoryRoutes.js";
@@ -21,10 +31,32 @@ import productRouter from "./routes/productRoutes.js";
 app.use("/api/v1/auth",userRoute)
 app.use("/api/v1/product",productRouter)
 app.use("/api/v1/category",categoryRouter)
-// middlewares
 
 // port
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`);
+
 })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null,  file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage });
+  
+  // POST route to handle image upload
+  app.post('/api/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
+    res.json({ imageUrl: `http://localhost:${port}/${req.file.path}` });
+  });
+
+
+
+
