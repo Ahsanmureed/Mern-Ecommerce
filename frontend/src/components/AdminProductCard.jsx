cff976cfa972c7fdc5863af7b9333f90e44bdeb4
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-05-18 15:53:31 +0500

    Update AdminProductCard.jsx

--- frontend/src/components/AdminProductCard.jsx
+++ frontend/src/components/AdminProductCard.jsx
@@ -6,7 +6,7 @@ const AdminProductCard = ({name,photo,price}) => {
   console.log(photo);
   return (
     <div className=' border border-transparent bg-gray-50 p-6 relative '>
-      <img className=' mx-auto w-44 h-48'  src={`http://localhost:4000/download/${photo}`} alt="no" />
+      <img className=' mx-auto w-44 h-48'  src={`${import.meta.env.VITE_URL}/download/${photo}`} alt="no" />
       <h1 className=' font-semibold'>{name.substring(0, 30)}..</h1>
       <h1 className=' font-bold'>{price}$ </h1>
       <MdModeEdit className=' absolute bottom-7 right-4 text-[21px] cursor-pointer' />
