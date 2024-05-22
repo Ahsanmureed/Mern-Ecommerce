1012a7dff2c5765bde569d6cf6cdbe3cda824625
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2024-05-22 16:40:44 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.static("public"))
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:["https://mern-ecommerce-dqm5.vercel.app","http://localhost:5173"],
+    origin:"https://mern-ecommerce-dqm5.vercel.app",
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials:true,
     
