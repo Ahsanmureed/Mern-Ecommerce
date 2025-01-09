d3beb502699574daeed55edabf26e3bbe8426d67
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2025-01-09 19:52:11 +0500

    loader fixed

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -13,7 +13,7 @@ useEffect(()=>{
 },[])
     return (
 
-      <div className=" mt-72 flex justify-center  items-center" role="status">
+      <div className=" mt-72 absolute flex justify-center  items-center" role="status">
         <h1></h1>
       <svg aria-hidden="true" className="inline w-10 h-10 mr-2 text-gray-200 animate-spin fill-[#2d2d2e]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
