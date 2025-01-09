9754b45ba95d88c52edcc5aefd646743d9318d4d
Ahsanmureed <162133597+Ahsanmureed@users.noreply.github.com>
2025-01-09 20:14:15 +0500

    home image fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <h1 className=' text-center  text-4xl md:text-7xl font-semibold'>ARRIVALS SALES</h1>
             <button className='  uppercase mx-auto text-white py-2  mt-3  hover:transition hover:scale-95 hover:duration-150 px-4 rounded-md flex bg-black text-center mb-5'>Shop Now</button>
         </div>
-        <img className='     w-[65%] md:h-[86vh] h-[42vh]  md:w-[31vw]   ' src={bg} alt="" />
+        <img className='     w-[65%] md:h-[calc(100vh-5rem)] h-[42vh]  md:w-[31vw]   ' src={bg} alt="" />
         </div>
 
         <div>
